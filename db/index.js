ec4a2840333b5122a096f493d754dda68c60d998
Nicolás <nicoo.marsili@gmail.com>
2022-04-22 19:46:37 -0300

    Comments Mutation

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose');
 
 
 const connectDB = async() => {
-    await mongoose.connect('mongodb://localhost/graphql-blog', {
+    await mongoose.connect(process.env.MONGODB_LOCALHOST, {
         useNewUrlParser: true,
         serializeFunctions: true,
         useUnifiedTopology: true,
